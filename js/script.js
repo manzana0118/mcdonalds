174482d877071c52f171b871898da8bfb964f058
manzana00 <dev.manzana00@gmail.com>
2022-04-23 09:40:42 +0900

    모달창 추가

--- js/script.js
+++ js/script.js
@@ -8,6 +8,20 @@ $(document).ready(function () {
         }, 500);
     });
 
+    // 모달창
+    let modal_close = $('.modal-close');
+    let modal = $('.modal');
+
+    modal_close.click(function () {
+        modal.hide();
+    });
+
+    let modal_bt = $('.modal-bt');
+    modal_bt.click(function () {
+        modal.show();
+    });
+
+
     // 이미지 슬라이드 영역
     let slider_line = $('.sw-slider-line'); // 늘어나는 라인 span
     let slide_delay = 3000;
